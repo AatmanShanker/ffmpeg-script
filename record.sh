3752bc7d88a2c4b386ed52d29f0a13b95cbce9f8
AatmanShanker <codekrishna2008@gmail.com>
2022-02-01 22:30:52 +0530

    added two input methods

--- record.sh
+++ record.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-#Creator: ErrorCreator
-#Script:
+printf "Type v vor video only and a for audio and video both: "
+read rectype
 
-printf "Enter filename (Make sure to add file format) :"
-read filename
-echo $filename
+if [ $rectype = "v" ]
+then
+	printf "Enter filename (Make sure to enter the file format): "
+	read vid_filename
+	ffmpeg -f x11grab -s 1366x768 -i :0.0 $vid_filename
 
-ffmpeg -f x11grab -s 1366x768 -i :0.0 -f alsa -ac 1 -i default $filename
+elif [ $rectype = "a" ]
+then
+	printf "Enter filename (Make sure to enter the file format): "
+	read filename
+	ffmpeg -f x11grab -s 1366x768 -i :0.0 -f alsa -ac 1 -i default $filename
+fi
